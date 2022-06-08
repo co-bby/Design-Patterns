d9e43737642411a1e7b5e23f9b63351129917329
co-bby <nkumsagoefredrick@gmail.com>
2022-06-08 14:17:33 -0400

    observer pattern

---
+++ observer.js
@@ -0,0 +1,25 @@
+/* With the observer pattern we  can subscribe certain objects , the observers , to
+ another object , calledd the observable  Whenever an events occurs the observable notifies all its observes!*/
+
+class Observable {
+  constructor() {
+    /* obaervers is an array that will get notify when a certain events occurs
+     */
+    this.observers = [];
+  }
+
+  /*a method inorder to add observers to the  observers list*/
+  subscribe(func) {
+    this.observers.push(func);
+  }
+
+  /* a method inorder to emove observes from the observers list*/
+  unsubscribe() {
+    this.observers = this.observers.filter((observer) => observer !== func);
+  }
+
+  /* a method to notify all onservers whenever a specific event occurs */
+  notify(data) {
+    this.observers.forEach((observer) => observer(data));
+  }
+}
